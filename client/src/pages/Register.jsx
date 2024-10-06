452c269dfe1220bff4676d9d2f4ed4aec8807f27
hpipl <hpiplodwala110@gmail.com>
2024-10-06 01:25:05 -0500

    register updated

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -9,14 +9,10 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-
-
 const Login = () => {
-  const [ showPassword, setShowPassword ] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
-  const [ token, setToken ] = useState(JSON.parse(localStorage.getItem("auth")) || "");
-
-
+  const [token, setToken] = useState(JSON.parse(localStorage.getItem("auth")) || "");
 
   const handleRegisterSubmit = async (e) => {
     e.preventDefault();
@@ -25,37 +21,40 @@ const Login = () => {
     let email = e.target.email.value;
     let password = e.target.password.value;
     let confirmPassword = e.target.confirmPassword.value;
+    let weight = e.target.weight.value;
+    let height = e.target.height.value;
+    let age = e.target.age.value;
+    let bloodGroup = e.target.bloodGroup.value;
 
-    if(name.length > 0 && lastname.length > 0 && email.length > 0 && password.length > 0 && confirmPassword.length > 0){
-
-      if(password === confirmPassword){
+    if (name.length > 0 && lastname.length > 0 && email.length > 0 && password.length > 0 && confirmPassword.length > 0 && weight.length > 0 && height.length > 0 && age.length > 0 && bloodGroup.length > 0) {
+      if (password === confirmPassword) {
         const formData = {
           username: name + " " + lastname,
           email,
-          password
+          password,
+          weight,
+          height,
+          age,
+          bloodGroup,
         };
-        try{
-        const response = await axios.post("http://localhost:3000/api/v1/register", formData);
-         toast.success("Registration successfull");
-         navigate("/login");
-       }catch(err){
-         toast.error(err.message);
-       }
-      }else{
+        try {
+          const response = await axios.post("http://localhost:3000/api/v1/register", formData);
+          toast.success("Registration successful");
+          navigate("/login");
+        } catch (err) {
+          toast.error(err.message);
+        }
+      } else {
         toast.error("Passwords don't match");
       }
-    
-
-    }else{
+    } else {
       toast.error("Please fill all inputs");
     }
-
-
-  }
+  };
 
   useEffect(() => {
-    if(token !== ""){
-      toast.success("You already logged in");
+    if (token !== "") {
+      toast.success("You are already logged in");
       navigate("/dashboard");
     }
   }, []);
@@ -74,18 +73,20 @@ const Login = () => {
             <h2>Welcome to our website!</h2>
             <p>Please enter your details</p>
             <form onSubmit={handleRegisterSubmit}>
-            <input type="text" placeholder="Name" name="name" required={true} />
-            <input type="text" placeholder="Lastname" name="lastname" required={true} />
+              <input type="text" placeholder="Name" name="name" required={true} />
+              <input type="text" placeholder="Lastname" name="lastname" required={true} />
               <input type="email" placeholder="Email" name="email" required={true} />
+              <input type="text" placeholder="Weight (kg)" name="weight" required={true} />
+              <input type="text" placeholder="Height (cm)" name="height" required={true} />
+              <input type="number" placeholder="Age" name="age" required={true} />
+              <input type="text" placeholder="Blood Group" name="bloodGroup" required={true} />
               <div className="pass-input-div">
                 <input type={showPassword ? "text" : "password"} placeholder="Password" name="password" required={true} />
-                {showPassword ? <FaEyeSlash onClick={() => {setShowPassword(!showPassword)}} /> : <FaEye onClick={() => {setShowPassword(!showPassword)}} />}
-                
+                {showPassword ? <FaEyeSlash onClick={() => { setShowPassword(!showPassword) }} /> : <FaEye onClick={() => { setShowPassword(!showPassword) }} />}
               </div>
               <div className="pass-input-div">
                 <input type={showPassword ? "text" : "password"} placeholder="Confirm Password" name="confirmPassword" required={true} />
-                {showPassword ? <FaEyeSlash onClick={() => {setShowPassword(!showPassword)}} /> : <FaEye onClick={() => {setShowPassword(!showPassword)}} />}
-                
+                {showPassword ? <FaEyeSlash onClick={() => { setShowPassword(!showPassword) }} /> : <FaEye onClick={() => { setShowPassword(!showPassword) }} />}
               </div>
               <div className="register-center-buttons">
                 <button type="submit">Sign Up</button>
@@ -96,7 +97,6 @@ const Login = () => {
               </div>
             </form>
           </div>
-
           <p className="login-bottom-p">
             Already have an account? <Link to="/login">Login</Link>
           </p>
